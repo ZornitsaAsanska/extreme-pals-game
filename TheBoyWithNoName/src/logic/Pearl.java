592fe5f69b623a6b148c4d3d71e40448907af733
Andrew <andrew.co.robertson@gmail.com>
2019-03-30 18:40:03 +0000

    give pearl class image

--- TheBoyWithNoName/src/logic/Pearl.java
+++ TheBoyWithNoName/src/logic/Pearl.java
@@ -1,12 +1,30 @@
 package logic;
 
+import java.awt.Image;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import intermediary.Settings;
+
 public class Pearl extends Block{
 	private boolean alive;
+	private Image pic;
+	
+
 	
 
 	public Pearl(int row, int col, Tile tile) {
 		super(row, col, tile);
 		alive = true;
+		
+	    try {
+	        this.pic = ImageIO.read(getClass().getResource(Settings.tileImage("Pearl.png")));
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
+
+	    this.pic = pic.getScaledInstance(Settings.TILE_SIZE, Settings.TILE_SIZE, Image.SCALE_SMOOTH);
 	}
 	
 	public boolean isAlive() {
